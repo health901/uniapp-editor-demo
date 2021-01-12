3c0dd3dbb9cbdd3b14ec84c596b33fb4b280f917
health901 <health901@163.com>
2021-01-12 15:53:10 +0800

    组件更新

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,12 +1,15 @@
 <template>
     <view class="content">
         <view class="text-area" v-if="!showEditor">
-            <view>简介：</text>
+            <view>
+                <text>简介：</text>
                 <view auto-height v-html="html" class="html"></view>
                 <button @tap="openEditor">编辑</button>
+                <button @tap="changeHtml(1)" style="margin-top: 10rpx;">设置HTML-1</button>
+                <button @tap="changeHtml(2)" style="margin-top: 10rpx;">设置HTML-2</button>
             </view>
         </view>
-        <meditor v-else class="editor" @cancel="hideEditor" @save="saveEditor" :html="html"
+        <meditor v-else class="editor" @cancel="hideEditor" @save="saveEditor" v-model="html"
             :imageUploader="uploadImg" :muiltImage="true"></meditor>
     </view>
 </template>
@@ -20,7 +23,8 @@
         data() {
             return {
                 showEditor:false,
-                html: '<b style="color:#ff0000">Hello</b>'
+                html: '<b style="color:#ff0000">HTML 片段1</b>',
+                html2:'<b style="color:#ff0000">HTML 片段2</b>'
             }
         },
         methods: {
@@ -31,12 +35,17 @@
                 this.showEditor = false
             },
             saveEditor: function(e) {
-                this.html = e.html
+                // v-model已实现赋值
+                // this.html = e.html
                 this.hideEditor()
             },
             uploadImg: function(img, callback) {
                 //上传图片逻辑,将图片链接传给回调函数
                 callback(img)
+            },
+            changeHtml(i){
+                //从远程获取数据赋值
+                this.html = i == 1 ? this.html : this.html2
             }
         }
     }
